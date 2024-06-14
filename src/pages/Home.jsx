e869cae973cbf7e5729d0b20fd1bd34aff80a0fb
JeongminJamie <sksrhcalsu@gmail.com>
2024-06-14 12:34:06 +0900

    working on google and normal usrs data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,11 @@ import React, { useEffect } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { googleSigninActions } from "../store/googleSignin-slice";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   // 구글 로그인 state
   const isGoogleClicked = useSelector(
@@ -19,26 +21,42 @@ const Home = () => {
     (state) => state.signin.isNormalLoginClicked
   );
 
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        // 로컬 스토리지에서 UID를 가져옵니다.
-        const uid = localStorage.getItem("uid");
+  // 구글 유저 데이터 가져오기
+  const fetchGoogleUserData = async () => {
+    try {
+      // 로컬 스토리지에서 UID를 가져옵니다.
+      const uid = localStorage.getItem("uid");
 
-        if (uid) {
-          // 서버에 GET 요청을 보냅니다.
-          const response = await axios.get(
-            `http://localhost:8080/firebase/auth/user/${uid}`
-          );
-          // 서버에서 받은 사용자 데이터를 상태 변수에 저장합니다.
-          dispatch(googleSigninActions.setGoogleUserData(response.data));
-        }
-      } catch (error) {
-        console.error("Error fetching user data:", error);
+      if (uid) {
+        // 서버에 GET 요청을 보냅니다.
+        const response = await axios.get(
+          `http://localhost:8080/firebase/auth/user/${uid}`
+        );
+        // 서버에서 받은 사용자 데이터를 상태 변수에 저장합니다.
+        dispatch(googleSigninActions.setGoogleUserData(response.data));
       }
-    };
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
 
-    fetchUserData();
+  // 일반 유저 데이터 가져오기
+  const fetchNormalUserData = async () => {
+    try {
+      const token = localStorage.getItem("token");
+    } catch (error) {
+      console.error("기본 로그인 사용자 정보를 받아오지 못했습니다");
+    }
+  };
+
+  useEffect(() => {
+    if (!isGoogleClicked && !isNormalLoginClicked) {
+      navigate("/");
+    } else if (isGoogleClicked) {
+      fetchGoogleUserData();
+    } else {
+      fetchNormalUserData();
+    }
   }, [dispatch]);
 
   return (
--- src/store/signup-slice.js
+++ src/store/signup-slice.js
@@ -4,7 +4,7 @@ export const postRegisterEmailPasswordToServer = (registrationData) => {
   return async (dispatch) => {
     try {
       // 데이터를 서버에 보낼 엔드포인트 URL
-      const url = "http://localhost:8080/users/register";
+      const url = "http://localhost:8080/register";
 
       // 서버로 전송할 데이터와 요청 옵션 설정
       const requestOptions = {
