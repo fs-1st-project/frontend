b5ae3f3b8249ae025e3f9e0971c4c8bd8035d683
JeongminJamie <sksrhcalsu@gmail.com>
2024-06-15 01:03:01 +0900

    show user profile info on home after signin in with normal user

--- src/component/NormalProfile/NormalProfile.jsx
+++ src/component/NormalProfile/NormalProfile.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const NormalProfile = () => {
-  return <div>일반 로그인 유저 프로필</div>;
+  const normalUserData = useSelector((state) => state.signin.normalUserData);
+  const isLoading = useSelector((state) => state.signin.isLoading);
+
+  if (isLoading) {
+    return <p>일반 로그인 유저 데이터를 불러오는 중입니다...</p>;
+  }
+
+  if (!normalUserData) {
+    return <p>일반 로그인 유저 정보가 없습니다.</p>;
+  }
+
+  return (
+    <div>
+      {normalUserData.profileBackgroundPicture ? (
+        <img
+          src={`data:image/jpeg;base64,${normalUserData.profileBackgroundPicture}`}
+          alt="User Background"
+        />
+      ) : (
+        <p>No background picture</p>
+      )}
+      {normalUserData.profilePicture ? (
+        <img
+          src={`data:image/jpeg;base64,${normalUserData.profilePicture}`}
+          alt="User Avatar"
+        />
+      ) : (
+        <p>No profile picture</p>
+      )}
+      <h2> {normalUserData.fullName}</h2>
+      <p>{normalUserData.introduction}</p>
+    </div>
+  );
 };
 
 export default NormalProfile;
