92f0e129db9a7284a539a11e79e7dc9bdfda5455
JeongminJamie <sksrhcalsu@gmail.com>
2024-06-16 15:45:59 +0900

    post styles

--- src/pages/Home/Post.jsx
+++ src/pages/Home/Post.jsx
@@ -3,7 +3,15 @@ import React from "react";
 import "./Post.css";
 
 const Post = () => {
-  return <div className="post-container">ㅎㅎㅎ</div>;
+  return (
+    <div className="post-container">
+      <div className="post-top"></div>
+      <div className="post-post-owner"></div>
+      <div className="post-contents"></div>
+      <div className="post-comments-count"></div>
+      <div className="post-bottom-buttons"></div>
+    </div>
+  );
 };
 
 export default Post;
