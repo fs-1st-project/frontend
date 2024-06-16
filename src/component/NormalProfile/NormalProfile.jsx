da0286f3bfe07c44d8c84e0a9dfd9f4ee5ba2879
gongjuyeonhee <yyeonhee912@naver.com>
2024-06-16 15:38:51 +0900

    insert data googleUser, nomalUser profile

--- src/component/NormalProfile/NormalProfile.jsx
+++ src/component/NormalProfile/NormalProfile.jsx
@@ -1,39 +1,64 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import "../../pages/Home/Home.css";
+import "./NormalProfile.css";
+import { Route, Link } from "react-router-dom";
 
 const NormalProfile = () => {
   const normalUserData = useSelector((state) => state.signin.normalUserData);
-  const isLoading = useSelector((state) => state.signin.isLoading);
+  const normalLoading = useSelector((state) => state.signin.normalLoading);
 
-  if (isLoading) {
-    return <p>일반 로그인 유저 데이터를 불러오는 중입니다...</p>;
+  if (normalLoading) {
+    return <p>데이터를 불러오는 중입니다...</p>;
   }
 
   if (!normalUserData) {
-    return <p>일반 로그인 유저 정보가 없습니다.</p>;
+    return <p>사용자 정보가 없습니다.</p>;
   }
 
   return (
     <div className="home-body_profile">
-      {normalUserData.profileBackgroundPicture ? (
-        <img
-          src={`data:image/jpeg;base64,${normalUserData.profileBackgroundPicture}`}
-          alt="User Background"
-        />
-      ) : (
-        <p>No background picture</p>
-      )}
-      {normalUserData.profilePicture ? (
-        <img
-          src={`data:image/jpeg;base64,${normalUserData.profilePicture}`}
-          alt="User Avatar"
-        />
-      ) : (
-        <p>No profile picture</p>
-      )}
-      <h2> {normalUserData.fullName}</h2>
-      <p>{normalUserData.introduction}</p>
+      <div className="Main-profile">
+        <div className="user-background-picture">
+          {normalUserData.profileBackgroundPicture ? (
+            <img
+              src={`data:image/jpeg;base64,${normalUserData.profileBackgroundPicture}`}
+              alt="User Background"
+              className="background-image"
+            />
+          ) : (
+            <img
+              src="https://th.bing.com/th/id/R.482217a0290b9a99687f84df3bc52dcf?rik=bzq1HZD3Qi1EOA&riu=http%3a%2f%2fdata.1freewallpapers.com%2fdownload%2fdream-house.jpg&ehk=JNLcBFl%2fLmMeKee508BFM6WcARgKV1MO4dIkrJsmasQ%3d&risl=&pid=ImgRaw&r=0"
+              alt="Default Background"
+              className="background-image"
+            />
+          )}
+        </div>
+        <div className="user-picture">
+          {normalUserData.profilePicture ? (
+            <img
+              src={`data:image/jpeg;base64,${normalUserData.profilePicture}`}
+              alt="User Avatar"
+            />
+          ) : (
+            <img
+              src="https://cdn-lostark.game.onstove.com/uploadfiles/user/2021/04/06/637533445557572173.png"
+              alt="Default user-picture"
+              className="picture-image"
+            />
+          )}
+        </div>
+        <div className="user-descript-box">
+          <div className="user-name">
+            <Link
+              to={`/profile/${encodeURIComponent(normalUserData.fullName)}`}
+            >
+              {normalUserData.fullName}
+            </Link>
+          </div>
+          <div className="user-introduction">{normalUserData.introduction}</div>
+        </div>
+      </div>
     </div>
   );
 };
